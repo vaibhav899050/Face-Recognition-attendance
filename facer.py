8b094ed4799799a672153ee0c6d022e06fccd1ac
Vaibhav <vg899050@gmail.com>
2022-12-05 13:57:49 +0530

    Initial commit

---
+++ facer.py
@@ -0,0 +1,11 @@
+import face_recognition_models
+import cv2
+import numpy as np
+import os
+import csv
+from datetime import datetime
+
+
+videocap = cv2.VideoCapture(0)
+
+img1 = face_recognition_models.
